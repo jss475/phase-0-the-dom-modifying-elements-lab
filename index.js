61bc1432a7bb9d74cd2ef52f6bffb8f736df76c6
JosephShin <joespam9999@gmail.com>
2022-04-08 14:10:17 -0400

    commit tree 04940e7764e638c8c635f8481d37aa0550b82ef8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+main.remove()
+
+const h1Element = document.createElement('h1')
+
+
+// document.querySelector('h1').id = 'victory'
+// document.getElementById('victory').innerHTML = 'Joe is the champion'
+// const newHeader = document.getElementById('victory')
+
+const newHeader = document.createElement('h1')
+
+debugger
+newHeader.textContent  = 'Joe is the champion'
+newHeader.id = "victory"
+document.body.append(newHeader)
